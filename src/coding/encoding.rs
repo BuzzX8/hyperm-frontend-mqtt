88f35ef1c09daba6c6d2e3c772321e393815e60f
Maxim <maxim.chasnik@gmail.com>
2023-04-10 17:59:00 +0300

    Added encode_str

--- src/coding/encoding.rs
+++ src/coding/encoding.rs
@@ -3,6 +3,7 @@ use std::mem::size_of;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum EncodingError {
     BufferTooSmall,
+    StringTooLong
 }
 
 pub fn encode_u16(buffer: &mut [u8], value: u16) -> Result<(), EncodingError> {
@@ -25,6 +26,26 @@ pub fn encode_u32(buffer: &mut [u8], value: u32) -> Result<(), EncodingError> {
     buffer[1] = (value >> 16) as u8;
     buffer[2] = (value >> 8) as u8;
     buffer[3] = value as u8;
+
+    Ok(())
+}
+
+pub fn encode_str(buffer: &mut [u8], str: &str) -> Result<(), EncodingError> {
+    let str = str.as_bytes();
+
+    if str.len() > u16::MAX as usize{
+        return Err(EncodingError::StringTooLong);
+    }
+
+    if str.len() + size_of::<u16>() > buffer.len() {
+        return  Err(EncodingError::BufferTooSmall);
+    }
+
+    let len = str.len() as u16;
+
+    encode_u16(buffer, len)?;
+    let buffer = &mut buffer[2..];
+    buffer[..str.len()].copy_from_slice(str);
 
     Ok(())
 }
@@ -80,5 +101,21 @@ mod encode_u32_tests {
 
         assert!(result.is_err());
         assert_eq!(EncodingError::BufferTooSmall, result.unwrap_err());
+    }
+}
+
+#[cfg(test)]
+mod encode_str_tests {
+    use super::*;
+
+    #[test]
+    fn encode_str_writes_str_to_buffer() {
+        let mut buffer = [0u8; 10];
+        let str = &[0x41u8, 0xF0, 0xAA, 0x9B, 0x94];
+
+        let result = encode_str(&mut buffer, std::str::from_utf8(str).unwrap());
+
+        assert!(result.is_ok());
+        assert_eq!(&[0x00u8, 0x05, 0x41, 0xF0, 0xAA, 0x9B, 0x94], &buffer[..7]);
     }
 }
